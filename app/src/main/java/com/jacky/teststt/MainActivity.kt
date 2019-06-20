d468b888f91a7529413c13d34a97c8dcf3738049
Jacky Tsang <szechunt@gmail.com>
2019-06-20 17:31:29 +0800

    trigger voice recognition

--- app/src/main/java/com/jacky/teststt/MainActivity.kt
+++ app/src/main/java/com/jacky/teststt/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.jacky.teststt
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val SPEECH: Int = 100
+    private val SIMPLE_NAME: String = MainActivity::class.java.name
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        voice.setOnClickListener {
+            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivityForResult(intent, SPEECH)
+            } else {
+                Log.e(SIMPLE_NAME, "Your device doesn't support speech input")
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            SPEECH -> {
+                if (resultCode == RESULT_OK) {
+                    val result = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                    speech_as_text.text = result?.get(0)
+                }
+            }
+        }
     }
 }
